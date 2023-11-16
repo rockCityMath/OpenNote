18ed29eabd6c600487332f892d95d95efe76effb
ldeltastreaml <70774713+ldeltastreaml@users.noreply.github.com>
2023-11-16 12:45:14 -0600

    Changed Icon File Path
    Changed to the correct one??? not sure why i pushed what it was before

--- Modules/BuildUI.py
+++ Modules/BuildUI.py
@@ -85,23 +85,23 @@ def build_menubar(editor):
     file = editor.menuBar().addMenu('&File')
     plugins = editor.menuBar().addMenu('&Plugins')
 
-    new_file = build_action(editor, 'assets/icons/svg_file_open', 'New Notebook', 'New Notebook', False)
+    new_file = build_action(editor, './Assets/icons/svg_file_open', 'New Notebook', 'New Notebook', False)
     new_file.setShortcut(QKeySequence.StandardKey.New)
     new_file.triggered.connect(lambda: new(editor))
 
-    open_file = build_action(editor, 'assets/icons/svg_file_open', 'Open Notebook', 'Open Notebook', False)
+    open_file = build_action(editor, './Assets/icons/svg_file_open', 'Open Notebook', 'Open Notebook', False)
     open_file.setShortcut(QKeySequence.StandardKey.Open)
     open_file.triggered.connect(lambda: load(editor))
 
-    save_file = build_action(editor, 'assets/icons/svg_file_save', 'Save Notebook', 'Save Notebook', False)
+    save_file = build_action(editor, './Assets/icons/svg_file_save', 'Save Notebook', 'Save Notebook', False)
     save_file.setShortcut(QKeySequence.StandardKey.Save)
     save_file.triggered.connect(lambda: save(editor))
 
-    save_fileAs = build_action(editor, 'assets/icons/svg_file_save', 'Save Notebook As...', 'Save Notebook As', False)
+    save_fileAs = build_action(editor, './Assets/icons/svg_file_save', 'Save Notebook As...', 'Save Notebook As', False)
     save_fileAs.setShortcut(QKeySequence.fromString('Ctrl+Shift+S'))
     save_fileAs.triggered.connect(lambda: saveAs(editor))
 
-    add_widget = build_action(editor, 'assets/icons/svg_question', 'Add Custom Widget', 'Add Custom Widget', False)
+    add_widget = build_action(editor, './Assets/icons/svg_question', 'Add Custom Widget', 'Add Custom Widget', False)
 
     file.addActions([new_file, open_file, save_file, save_fileAs])
     plugins.addActions([add_widget])
@@ -116,11 +116,11 @@ def build_toolbar(editor):
     spacer = QWidget()
     spacer.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
 
-    toolbar_undo = build_action(toolbar, 'assets/icons/svg_undo', "undo", "undo", False)
+    toolbar_undo = build_action(toolbar, './Assets/icons/svg_undo', "undo", "undo", False)
     toolbar_undo.triggered.connect(editor.frameView.triggerUndo)
 
 
-    redo = build_action(toolbar, 'assets/icons/svg_redo', "redo", "redo", False)
+    redo = build_action(toolbar, './Assets/icons/svg_redo', "redo", "redo", False)
     
 
 
@@ -139,39 +139,39 @@ def build_toolbar(editor):
     # - Alternates between working and not working
     # - Textboxes do not remember settings like if font is toggled or current font size
 
-    bgColor = build_action(toolbar, 'assets/icons/svg_font_bucket', "Background Color", "Background Color", False)
+    bgColor = build_action(toolbar, './Assets/icons/svg_font_bucket', "Background Color", "Background Color", False)
     #bgColor.triggered.connect(lambda: openGetColorDialog(purpose = "background"))
     #current bug, alternates between activating and not working when using
     bgColor.triggered.connect(lambda: editorSignalsInstance.widgetAttributeChanged.emit(ChangedWidgetAttribute.BackgroundColor, QColorDialog.getColor()))
 
-    textboxColor = build_action(toolbar, 'assets/icons/svg_textboxColor', "Text Box Color", "Text Box Color", True)
+    textboxColor = build_action(toolbar, './Assets/icons/svg_textboxColor', "Text Box Color", "Text Box Color", True)
     textboxColor.toggled.connect(lambda: editorSignalsInstance.widgetAttributeChanged.emit(ChangedWidgetAttribute.TextboxColor, QColorDialog.getColor()))
 
     #defines font color icon appearance and settings
-    fontColor = build_action(toolbar, 'assets/icons/svg_font_color', "Font Color", "Font Color", False)
+    fontColor = build_action(toolbar, './Assets/icons/svg_font_color', "Font Color", "Font Color", False)
     fontColor.triggered.connect(lambda: openGetColorDialog(purpose = "font"))
 
-    bold = build_action(toolbar, 'assets/icons/bold', "Bold", "Bold", True)
+    bold = build_action(toolbar, './Assets/icons/bold', "Bold", "Bold", True)
     bold.toggled.connect(lambda: editorSignalsInstance.widgetAttributeChanged.emit(ChangedWidgetAttribute.FontBold, None))
 
-    italic = build_action(toolbar, 'assets/icons/italic.svg', "Italic", "Italic", True)
+    italic = build_action(toolbar, './Assets/icons/italic.svg', "Italic", "Italic", True)
     italic.toggled.connect(lambda: editorSignalsInstance.widgetAttributeChanged.emit(ChangedWidgetAttribute.FontItalic, None))
 
-    underline = build_action(toolbar, 'assets/icons/underline.svg', "Underline", "Underline", True)
+    underline = build_action(toolbar, './Assets/icons/underline.svg', "Underline", "Underline", True)
     underline.toggled.connect(lambda: editorSignalsInstance.widgetAttributeChanged.emit(ChangedWidgetAttribute.FontUnderline, None))
 
-    table = build_action(toolbar, 'assets/icons/svg_table', "Create Table", "Create Table", False)
+    table = build_action(toolbar, './Assets/icons/svg_table', "Create Table", "Create Table", False)
     table.triggered.connect(editor.frameView.toolbar_table)
 
-    hyperlink = build_action(toolbar, 'assets/icons/svg_hyperlink', "Hyperlink", "Hyperlink", False)
+    hyperlink = build_action(toolbar, './Assets/icons/svg_hyperlink', "Hyperlink", "Hyperlink", False)
     hyperlink.triggered.connect(editor.frameView.toolbar_hyperlink)
 
-    bullets = build_action(toolbar, 'assets/icons/svg_bullets', "Bullets", "Bullets", False)
+    bullets = build_action(toolbar, './Assets/icons/svg_bullets', "Bullets", "Bullets", False)
 
-    bullet_reg = build_action(toolbar, 'assets/icons/svg_bullets', "Bullet List", "Bullet List", False)
+    bullet_reg = build_action(toolbar, './Assets/icons/svg_bullets', "Bullet List", "Bullet List", False)
     bullet_reg.triggered.connect(lambda: editorSignalsInstance.widgetAttributeChanged.emit(ChangedWidgetAttribute.Bullet, None))
 
-    bullet_num = build_action(toolbar, 'assets/icons/svg_bullet_number', "Bullet List", "Bullet List", False)
+    bullet_num = build_action(toolbar, './Assets/icons/svg_bullet_number', "Bullet List", "Bullet List", False)
     bullet_num.triggered.connect(lambda: editorSignalsInstance.widgetAttributeChanged.emit(ChangedWidgetAttribute.Bullet_Num, None))
 
     '''
