54a46ff54e5fd361d231c2b943ecbcf2b92bb92f
benjamintran1 <145232360+benjamintran1@users.noreply.github.com>
2023-12-06 15:56:21 -0600

    Update snipping tool

--- Modules/Screensnip.py
+++ Modules/Screensnip.py
@@ -13,8 +13,12 @@ class SnippingWidget(QWidget):
 
     def __init__(self):
         super(SnippingWidget, self).__init__()
-        self.setWindowFlags(Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint)
-        self.setAttribute(Qt.WA_TranslucentBackground)
+
+        if platform == "linux":
+            self.setWindowFlags(Qt.FramelessWindowHint)
+            self.setAttributes(Qt.WA_TranslucentBackground)
+            
+        self.setWindowFlags(Qt.WindowStaysOnTopHint) 
         self.parent = None
         self.screen = QApplication.instance().primaryScreen()
         self.setGeometry(0, 0, self.screen.size().width(), self.screen.size().height())
@@ -25,44 +29,56 @@ class SnippingWidget(QWidget):
     def start(self, event_pos):
         print("SnippingWidget.start")
         SnippingWidget.is_snipping = True
+
+        if platform != "linux":
+            self.setWindowOpacity(0.3)
+
         QApplication.setOverrideCursor(QCursor(Qt.CrossCursor))
         self.event_pos = event_pos
         self.show()
         print("SnippingWidget.start done")
 
     def paintEvent(self, event):
-        qp = QPainter(self)
         if SnippingWidget.is_snipping:
             brush_color = (128, 128, 255, 100)
             lw = 3
+            opacity = 0.3
+
+            if platform != "linux":
+                self.setWindowOpacity(opacity)
+    
+            qp = QPainter(self)
             qp.setPen(QPen(QColor('black'), lw))
             qp.setBrush(QColor(*brush_color))
             rect = QRectF(self.begin, self.end)
             qp.drawRect(rect)
+        else:
+            self.begin = QPoint()
+            self.end = QPoint()
+            brush_color = (0, 0, 0, 0)
+            lw = 0
+            opacity = 0
 
     def mousePressEvent(self, event):
-        try:
-            self.begin = event.pos()
-            self.end = self.begin
-            self.update()
-        except Exception as e:
-            print(f"Error handling mouse press event: {e}")
+        self.begin = event.pos()
+        self.end = self.begin
+        self.update()
 
     def mouseMoveEvent(self, event):
-        try:
-            self.end = event.pos()
-            self.update()
-        except Exception as e:
-            print(f"Error handling mouse move event: {e}")
+        self.end = event.pos()
+        self.update()
 
     def mouseReleaseEvent(self, event):
         SnippingWidget.is_snipping = False
         QApplication.restoreOverrideCursor()
-        x1, y1, x2, y2 = self.getSnipCoordinates()
+        x1 = min(self.begin.x(), self.end.x())
+        y1 = min(self.begin.y(), self.end.y())
+        x2 = max(self.begin.x(), self.end.x())
+        y2 = max(self.begin.y(), self.end.y())
 
         self.repaint()
         QApplication.processEvents()
-        
+
         try:
             if platform == "darwin":
                 img = ImageGrab.grab(bbox=( (x1 ) * 2, (y1 + 55 ) * 2, (x2 ) * 2, (y2 + 55) * 2))
@@ -72,24 +88,13 @@ class SnippingWidget(QWidget):
             print(f"Error grabbing screenshot: {e}")
             img = None
 
+
         try:
             img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
         except:
-            print(f"Error converting screenshot to NumPy array: {e}")
             img = None
 
         if self.onSnippingCompleted is not None:
             self.onSnippingCompleted(img)
 
         self.close()
-
-    def getSnipCoordinates(self):
-        try:
-            x1 = min(self.begin.x(), self.end.x())
-            y1 = min(self.begin.y(), self.end.y())
-            x2 = max(self.begin.x(), self.end.x())
-            y2 = max(self.begin.y(), self.end.y())
-            return x1, y1, x2, y2
-        except Exception as e:
-            print(f"Error getting snip coordinates {e}")
-            return 0, 0, 0, 0    
