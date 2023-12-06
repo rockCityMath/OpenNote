e247b96e056f8fccf9d5b30d44fb2596faa89e08
ldeltastreaml <70774713+ldeltastreaml@users.noreply.github.com>
2023-12-06 13:32:25 -0600

    Merge two versions of screensnip
    added functionality between the various versions.

--- Modules/Screensnip.py
+++ Modules/Screensnip.py
@@ -13,19 +13,26 @@ class SnippingWidget(QWidget):
 
     def __init__(self):
         super(SnippingWidget, self).__init__()
-        self.setWindowFlags(Qt.WindowStaysOnTopHint)
+
+        if platform == "linux":
+            self.setWindowFlags(Qt.FramelessWindowHint)
+            self.setAttributes(Qt.WA_TranslucentBackground)
+            
+        self.setWindowFlags(Qt.WindowStaysOnTopHint) 
         self.parent = None
         self.screen = QApplication.instance().primaryScreen()
         self.setGeometry(0, 0, self.screen.size().width(), self.screen.size().height())
-        self.begin = QPoint()
-        self.end = QPoint()
+        self.begin = self.end = QPoint()
         self.onSnippingCompleted = None
         self.event_pos = None
 
     def start(self, event_pos):
         print("SnippingWidget.start")
         SnippingWidget.is_snipping = True
-        self.setWindowOpacity(0.3)
+
+        if platform != "linux":
+            self.setWindowOpacity(0.3)
+
         QApplication.setOverrideCursor(QCursor(Qt.CrossCursor))
         self.event_pos = event_pos
         self.show()
@@ -36,19 +43,21 @@ class SnippingWidget(QWidget):
             brush_color = (128, 128, 255, 100)
             lw = 3
             opacity = 0.3
+
+            if platform != "linux":
+                self.setWindowOpacity(opacity)
+    
+            qp = QPainter(self)
+            qp.setPen(QPen(QColor('black'), lw))
+            qp.setBrush(QColor(*brush_color))
+            rect = QRectF(self.begin, self.end)
+            qp.drawRect(rect)
         else:
             self.begin = QPoint()
             self.end = QPoint()
             brush_color = (0, 0, 0, 0)
             lw = 0
             opacity = 0
-
-        self.setWindowOpacity(opacity)
-        qp = QPainter(self)
-        qp.setPen(QPen(QColor('black'), lw))
-        qp.setBrush(QColor(*brush_color))
-        rect = QRectF(self.begin, self.end)
-        qp.drawRect(rect)
 
     def mousePressEvent(self, event):
         self.begin = event.pos()
@@ -70,11 +79,14 @@ class SnippingWidget(QWidget):
         self.repaint()
         QApplication.processEvents()
 
-        if platform == "darwin":
-            img = ImageGrab.grab(bbox=( (x1 ) * 2, (y1 + 55 ) * 2, (x2 ) * 2, (y2 + 55) * 2))
-        else:
-            img = ImageGrab.grab(bbox=(x1 + 10, y1 + 30, x2 + 10, y2 + 40))
-
+        try:
+            if platform == "darwin":
+                img = ImageGrab.grab(bbox=( (x1 ) * 2, (y1 + 55 ) * 2, (x2 ) * 2, (y2 + 55) * 2))
+            else:
+                img = ImageGrab.grab(bbox=(x1 + 10, y1 + 30, x2 + 10, y2 + 40))
+        except Exception as e:
+            print(f"Error grabbing screenshot: {e}")
+            img = None
 
 
         try:
